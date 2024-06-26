1e1ac65e31a6af4820d2deb334e1867f16c6f90f
di-photos <di-photos@live.cn>
2024-06-26 23:12:55 +0800

    update config

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Nasu",
+  base: '/vitehub/',
   description: "More Share More Fun",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
