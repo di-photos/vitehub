81d4bfd15f688cf8aa40fb8d18f1267bb0de7dac
di-photos <di-photos@live.cn>
2024-06-30 20:40:24 +0800

    update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -87,6 +87,7 @@ export default defineConfig({
     sidebar: [
       {
         text: '一、NAS硬件杂谈',
+        collapsed: false,
         items: [
           { text: 'NAS机箱选择', link: '/Hardware/NAS硬件杂谈：NAS机箱选择' },
           { text: '板U平台', link: '/Hardware/NAS硬件杂谈：板U平台' },
@@ -96,6 +97,7 @@ export default defineConfig({
       },
       {
         text: '二、NAS系统相关',
+        collapsed: true,
         items: [
           { text: '各Nas系统的分析对比', link: '/System/NAS系统相关：各Nas系统的分析对比' },
           { text: 'RAID阵列', link: '/System/NAS系统相关：RAID阵列' }
@@ -103,6 +105,7 @@ export default defineConfig({
       },
       {
         text: '三、NAS应用',
+        collapsed: true,
         items: [
           { text: 'PVE下安装WG-Easy', link: '/Application/NAS应用：PVE下安装WG-Easy' },
           { text: 'Unraid 引导盘制作Flash GUID为0000结尾的处理方法', link: '/Application/NAS应用：Unraid 引导盘制作Flash GUID为0000结尾的处理方法' },
@@ -111,6 +114,7 @@ export default defineConfig({
       },
       {
         text: '四、NAS使用指引',
+        collapsed: true,
         items: [
           { text: '物理群晖', link: '/Guide/NAS使用指引：物理群晖' },
           { text: 'Unrai及虚拟群晖', link: '/Guide/NAS使用指引：Unrai及虚拟群晖' }
