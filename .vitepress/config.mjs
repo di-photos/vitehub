5da24105f662e82d87fae3083c702f2099175994
di-photos <di-photos@live.cn>
2024-07-02 23:22:53 +0800

    update

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -12,29 +12,16 @@ export default defineConfig({
     siteTitle: 'NAS·U',
     search: {
       provider: 'local'
-    }
+    },
+    outline: {
+      level: [2,5],
+      label: '本文提纲'
+    },
     lastUpdated: true,
     lastUpdatedText: "最后更新",        
     nav: [    
-      { text: 'NAS硬件',
-        items: [
-          {
-            text: '【01】 NAS机箱',
-            link: '/Hardware/NAS Case'
-          },
-          {
-            text: '【02】 板U平台', 
-            link: '/Hardware/Motherboard and CPU'
-          },
-          {
-            text: '【03】 机箱风道/风扇', 
-            link: '/Hardware/Case Duct and Fan'
-          },
-          {
-            text: '【04】 电源', 
-            link: '/Hardware/Power Supply'
-          }
-        ]
+      { text: '首页',
+        link: '/index'
       },
       { text: 'NAS系统',
         items: [
@@ -104,37 +91,38 @@ export default defineConfig({
             { text: '★★★★☆ | 宝藏盒Pro', link: '/Hardware/Treasure PRO' }
           ]
           },
-          { text: '【1.02】-板U平台', link: '/Hardware/Motherboard and CPU' },          
-          { text: '【1.02】-板U平台', link: '/Hardware/Motherboard and CPU' },
-          { text: '【1.03】-机箱风道/风扇', link: '/Hardware/Case Duct and Fan' },
+          { text: 'CPU', link: '/Hardware/Motherboard and CPU' },          
+          { text: '主板', link: '/Hardware/Motherboard and CPU' },
+          { text: '内存', link: '/Hardware/Memory' },
+          { text: '机箱风道/风扇', link: '/Hardware/Case Duct and Fan' },
           { text: '电源', link: '/Hardware/Power Supply' },  
         ]
       },
       {
-        text: '【2.0】 NAS系统',
+        text: 'NAS系统',
         collapsed: true,
         items: [
-          { text: '【2.01】-各Nas系统的分析对比', link: '/System/Comparative analysis of Nas systems' },
-          { text: '【2.02】-常见的RAID阵列', link: '/System/Raid Array' }
+          { text: '各Nas系统的分析对比', link: '/System/Comparative analysis of Nas systems' },
+          { text: '常见的RAID阵列', link: '/System/Raid Array' }
         ]
       },
       {
-        text: '【3.0】 NAS应用',
+        text: 'NAS应用',
         collapsed: true,
         items: [
-          { text: '【3.01】-PVE下安装WG-Easy', link: '/Application/Installation of WG-Easy under PVE' },
-          { text: '【3.02】-Unraid 引导盘制作Flash GUID为0000结尾的处理方法', link: '/Application/Flash GUID ends in 0000 processing method' },
-          { text: '【3.03】-MoviePilot 安装与使用', link: '/Application/MoviePilot Installation and Use' },
-          { text: '【3.04】-Unraid部署的思源笔记不能启动问题', link: '/Application/SiYuan Notes' },
-          { text: '【3.05】-解决Unraid的 Docker 镜像无法下载问题', link: '/Application/Docker image downloaded' }
+          { text: 'PVE下安装WG-Easy', link: '/Application/Installation of WG-Easy under PVE' },
+          { text: 'Unraid 引导盘制作Flash GUID为0000结尾的处理方法', link: '/Application/Flash GUID ends in 0000 processing method' },
+          { text: 'MoviePilot 安装与使用', link: '/Application/MoviePilot Installation and Use' },
+          { text: 'Unraid部署的思源笔记不能启动问题', link: '/Application/SiYuan Notes' },
+          { text: '解决Unraid的 Docker 镜像无法下载问题', link: '/Application/Docker image downloaded' }
         ]
       },
       {
-        text: '【4.0】 使用指引',
+        text: '使用指引',
         collapsed: true,
         items: [
-          { text: '【4.01】-物理群晖', link: '/Guide/Synology Installation' },
-          { text: '【4.02】-Unrai及虚拟群晖', link: '/Guide/Unrai and Virtual Synology' }
+          { text: '物理群晖', link: '/Guide/Synology Installation' },
+          { text: 'Unrai及虚拟群晖', link: '/Guide/Unrai and Virtual Synology' }
         ]
       }      
     ],
